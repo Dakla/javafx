084027c23b51251f635230c2669ec1b1b47cc887
Dakla <35534554+Dakla@users.noreply.github.com>
2020-01-23 19:27:34 +0800

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller {
+
+    @FXML
+    public Button loginLoginBtn;
+
+    @FXML
+    public Button loginRegBnt;
+
+    @FXML
+    private ResourceBundle resourceBundle;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField loginLogin;
+
+    @FXML
+    private PasswordField loginPwd;
+
+    @FXML
+    void initialize() {
+        loginLoginBtn.setOnAction(event -> {
+            System.out.println("Войти");
+        });
+    }
+}
